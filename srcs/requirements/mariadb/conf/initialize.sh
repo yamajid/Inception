ddb653622d4f3bd0eba2654c2d86cc6e895cbd87
Younes Amajid <yamajid@e3r5p4.1337.m>
2024-08-29 11:47:02 +0100

    ()

---
+++ srcs/requirements/mariadb/conf/initialize.sh
@@ -0,0 +1,43 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    initialize.sh                                      :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: yamajid <yamajid@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/08/14 09:54:52 by yamajid           #+#    #+#              #
+#    Updated: 2024/08/29 10:31:11 by yamajid          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!/bin/bash
+
+set -e
+
+max_tries=10
+counter=0
+
+service mariadb start
+
+echo "Waiting for MariaDB to be ready..."
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+mysql -u root -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+
+while ! mysql -u root -e "SELECT 1" > /dev/null 2>&1; do
+    counter=$((counter+1))
+    if [ $counter -gt $max_tries ]; then
+        echo "MariaDB failed to start"
+        exit 1
+    fi
+    sleep 1
+done
+
+mysqladmin -u root -p${DB_PASS} shutdown
+
+echo "MariaDB is ready!"
+
+exec mysqld_safe --datadir='/var/lib/mysql' --bind-address=0.0.0.0 --port=3306
---
+++ srcs/requirements/wordpress/conf/wp_install.sh
@@ -0,0 +1,54 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    wp_install.sh                                      :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: yamajid <yamajid@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/08/10 15:01:11 by yamajid           #+#    #+#              #
+#    Updated: 2024/08/29 11:44:21 by yamajid          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!/bin/bash
+
+set -e
+
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+php wp-cli.phar --info
+
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+wp core download --path=/var/www/html/ --allow-root
+
+wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER \
+    --dbhost=$WORDPRESS_DB_HOST  --dbpass=$WORDPRESS_DB_PASSWORD --path=/var/www/html  --allow-root
+
+wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE \
+     --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD \
+     --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
+
+max_tries=10
+counter=0
+
+while mysqladmin ping -h"$DB_HOST" --silent;
+do
+    sleep 1
+    counter=$((counter + 1))
+    if [ $counter -ge $max_tries ]; then
+        echo "Error: MariaDB did not became ready in time."
+        exit 1
+    fi
+    echo "Attempt $counter: Waiting for MariaDB to be ready..."
+done
+
+wp user create $USER_NAME $USER_EMAIL --role=author --user_pass=$USER_PASS --allow-root
+
+sed -i '36 s*/run/php/php7.4-fpm.sock*9000*' /etc/php/7.4/fpm/pool.d/www.conf
+
+mkdir -p /run/php
+
+exec php-fpm7.4
