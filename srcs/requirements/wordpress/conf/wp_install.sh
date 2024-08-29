fae788c2b27c02be33909cf84cd61f6743af1d1e
Younes Amajid <yamajid@e3r5p4.1337.m>
2024-08-29 11:57:38 +0100

    ()

--- srcs/requirements/wordpress/conf/wp_install.sh
+++ srcs/requirements/wordpress/conf/wp_install.sh
@@ -6,7 +6,7 @@
 #    By: yamajid <yamajid@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/08/10 15:01:11 by yamajid           #+#    #+#              #
-#    Updated: 2024/08/29 11:44:21 by yamajid          ###   ########.fr        #
+#    Updated: 2024/08/29 11:54:30 by yamajid          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -24,6 +24,7 @@ mv wp-cli.phar /usr/local/bin/wp
 
 wp core download --path=/var/www/html/ --allow-root
 
+
 wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER \
     --dbhost=$WORDPRESS_DB_HOST  --dbpass=$WORDPRESS_DB_PASSWORD --path=/var/www/html  --allow-root
 
@@ -31,7 +32,7 @@ wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE \
      --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD \
      --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
 
-max_tries=10
+max_tries=15
 counter=0
 
 while mysqladmin ping -h"$DB_HOST" --silent;
