8c903632677e15c4197ff8fe6b67f5646f19bf14
Younes Amajid <yamajid@e3r5p4.1337.m>
2024-09-17 01:14:13 +0100

    ()

--- srcs/requirements/wordpress/conf/wp_install.sh
+++ srcs/requirements/wordpress/conf/wp_install.sh
@@ -6,7 +6,7 @@
 #    By: yamajid <yamajid@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/08/10 15:01:11 by yamajid           #+#    #+#              #
-#    Updated: 2024/08/30 14:26:35 by yamajid          ###   ########.fr        #
+#    Updated: 2024/09/02 19:25:32 by yamajid          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,7 +15,7 @@
 set -e
 
 cd /var/www/html
-
+    
 if [ ! -f "/usr/local/bin/wp" ]; then
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
@@ -28,7 +28,9 @@ wp core download --path=/var/www/html/ --allow-root
 
 wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER \
     --dbhost=$WORDPRESS_DB_HOST  --dbpass=$WORDPRESS_DB_PASSWORD --path=/var/www/html/  --allow-root
-
+    
+max_tries=15
+counter=0
 
 while mysqladmin ping -h"$WORDPRESS_DB_HOST" --silent;
 do
@@ -41,18 +43,14 @@ do
     echo "Attempt $counter: Waiting for MariaDB to be ready..."
 done
 
-
 wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE \
      --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD \
      --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
 
-max_tries=15
-counter=0
-
 
 wp user create $USER_NAME $USER_EMAIL --role=author --user_pass=$USER_PASSWORD --allow-root
 
-sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 0.0.0.0:9000|' /etc/php/7.4/fpm/pool.d/www.conf
 
 mkdir -p /run/php
 
